63e3ca399db1d4e7f4ba53036ae4676a75dbfe5c
Ruben Deisenroth <rdeisenroth@gmail.com>
2022-07-05 18:23:52 +0200

    Add reflection package, further cleanup necessary

--- algoutils-tutor/build.gradle.kts
+++ algoutils-tutor/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
@@ -19,6 +19,9 @@ dependencies {
     implementation("com.google.guava:guava:30.1.1-jre")
 
     implementation(project(":algoutils-student"))
+    implementation("fr.inria.gforge.spoon:spoon-core:10.0.0")
+    implementation("org.sourcegrade:docwatcher-api:0.1")
+    implementation("org.mockito:mockito-core:4.3.1")
 }
 
 tasks.named<Test>("test") {
