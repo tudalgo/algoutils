09e1fff6643122e2e68373e9262b9134a63207d8
Alexander Staeding <alexanderstaeding@gmail.com>
2022-07-07 23:06:38 +0200

    Remove redundant dependencies and move to version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,4 @@ plugins {
 }
 allprojects {
     apply(plugin = "java-library")
-    dependencies {
-        api("org.apache.commons:commons-math3:3.6.1")
-        implementation("com.google.guava:guava:31.1-jre")
-    }
 }
