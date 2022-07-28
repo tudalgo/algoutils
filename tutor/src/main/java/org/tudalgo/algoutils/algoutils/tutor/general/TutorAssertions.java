ee0604b0377c04fcaad4bc4c472069cd9878884c
Dustin Glaser <dustin.glaser@posteo.de>
2022-07-28 16:08:30 +0200

    add shortcuts

--- tutor/src/main/java/org/tudalgo/algoutils/algoutils/tutor/general/TutorAssertions.java
+++ tutor/src/main/java/org/tudalgo/algoutils/algoutils/tutor/general/TutorAssertions.java
@@ -1,11 +1,12 @@
 package org.tudalgo.algoutils.algoutils.tutor.general;
 
 import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
-import org.tudalgo.algoutils.student.CrashException;
+import org.opentest4j.AssertionFailedError;
 import org.tudalgo.algoutils.algoutils.tutor.general.call.Call;
 import org.tudalgo.algoutils.algoutils.tutor.general.call.CallImpl;
-import org.opentest4j.AssertionFailedError;
+import org.tudalgo.algoutils.student.CrashException;
 
 public class TutorAssertions {
 
@@ -25,5 +26,78 @@ public class TutorAssertions {
         } catch (Exception exception) {
             return new CallImpl.ExceptionalResult<>(exception);
         }
+    }
+
+    /**
+     * Shortcut for  {@link #call(Callable)} and {@link Call#assertNormal(Supplier)}.
+     *
+     * @param callable        see {@link #call(Callable)}
+     * @param messageSupplier see {@link Call#assertNormal(Supplier)}
+     * @param <R>             see {@link Call#assertNormal(Supplier)}
+     * @return see {@link Call#assertNormal(Supplier)}
+     */
+    public static <R> R assertNormal(Callable<R> callable, Supplier<String> messageSupplier) {
+        return call(callable).assertNormal(messageSupplier);
+    }
+
+    /**
+     * Shortcut for  {@link #call(Callable)} and {@link Call#assertNormal(String)}.
+     *
+     * @param callable see {@link #call(Callable)}
+     * @param message  see {@link Call#assertNormal(String)}
+     * @param <R>      see {@link Call#assertNormal(String)}
+     * @return see {@link Call#assertNormal(String)}
+     */
+    public static <R> R assertNormal(Callable<R> callable, String message) {
+        return call(callable).assertNormal(message);
+    }
+
+    /**
+     * Shortcut for  {@link #call(Callable)} and {@link Call#assertNormal()}.
+     *
+     * @param callable see {@link #call(Callable)}
+     * @param <R>      see {@link Call#assertNormal()}
+     * @return see {@link Call#assertNormal()}
+     */
+    public static <R> R assertNormal(Callable<R> callable) {
+        return call(callable).assertNormal();
+    }
+
+    /**
+     * Shortcut for  {@link #call(Callable)} and {@link Call#assertExceptional(Class, Supplier)}.
+     *
+     * @param callable        see {@link #call(Callable)}
+     * @param messageSupplier see {@link Call#assertExceptional(Class, Supplier)}
+     * @param <E>             see {@link Call#assertExceptional(Class, Supplier)}
+     * @return see {@link Call#assertExceptional(Class, Supplier)}
+     */
+    public static <E extends Exception> E assertExceptional(Callable<?> callable, Class<E> type,
+                                                            Supplier<String> messageSupplier) {
+        return call(callable).assertExceptional(type, messageSupplier);
+    }
+
+    /**
+     * Shortcut for  {@link #call(Callable)} and {@link Call#assertExceptional(Class, String)}.
+     *
+     * @param callable see {@link #call(Callable)}
+     * @param type     see {@link Call#assertExceptional(Class, String)}
+     * @param message  see {@link Call#assertExceptional(Class, String)}
+     * @param <E>      see {@link Call#assertExceptional(Class, String)}
+     * @return see {@link Call#assertExceptional(Class, String)}
+     */
+    public static <E extends Exception> E assertExceptional(Callable<?> callable, Class<E> type, String message) {
+        return call(callable).assertExceptional(type, message);
+    }
+
+    /**
+     * Shortcut for  {@link #call(Callable)} and {@link Call#assertExceptional(Class)}.
+     *
+     * @param callable see {@link #call(Callable)}
+     * @param type     see {@link Call#assertExceptional(Class)}
+     * @param <E>      see {@link Call#assertExceptional(Class)}
+     * @return see {@link Call#assertExceptional(Class)}
+     */
+    public static <E extends Exception> E assertExceptional(Callable<?> callable, Class<E> type) {
+        return call(callable).assertExceptional(type);
     }
 }
