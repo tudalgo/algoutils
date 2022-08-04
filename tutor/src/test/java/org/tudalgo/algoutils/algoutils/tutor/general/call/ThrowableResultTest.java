64f92caf19b08c7c98bd86ee58de289e94308c63
Dustin Glaser <dustin.glaser@posteo.de>
2022-08-04 18:52:13 +0200

    add ThrowableResultTest.java

---
+++ tutor/src/test/java/org/tudalgo/algoutils/algoutils/tutor/general/call/ThrowableResultTest.java
@@ -0,0 +1,35 @@
+package org.tudalgo.algoutils.algoutils.tutor.general.call;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.AssertionFailedError;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThrowableResultTest {
+
+    @Test
+    void testAssertThrows() {
+        var expected = new RuntimeException();
+        var result = new ThrowableResult<>(expected);
+        var actual = (RuntimeException) null;
+        actual = Assertions.assertDoesNotThrow(() -> result.assertThrows(RuntimeException.class));
+        assertSame(expected, actual);
+        actual = Assertions.assertDoesNotThrow(() -> result.assertThrows(RuntimeException.class, "test"));
+        assertSame(expected, actual);
+        actual = Assertions.assertDoesNotThrow(() -> result.assertThrows(RuntimeException.class, () -> "test"));
+        assertSame(expected, actual);
+    }
+
+    @Test
+    void assertNormal() {
+        var result = new ThrowableResult<>(new RuntimeException());
+        var actual = (AssertionFailedError) null;
+        actual = assertThrows(AssertionFailedError.class, result::assertNormal);
+        assertEquals("expected no throwable, but throwable of type RuntimeException was thrown", actual.getMessage());
+        actual = assertThrows(AssertionFailedError.class, () -> result.assertNormal("test"));
+        assertEquals("test: expected no throwable, but throwable of type RuntimeException was thrown", actual.getMessage());
+        actual = assertThrows(AssertionFailedError.class, () -> result.assertNormal(() -> "test"));
+        assertEquals("test: expected no throwable, but throwable of type RuntimeException was thrown", actual.getMessage());
+    }
+}
