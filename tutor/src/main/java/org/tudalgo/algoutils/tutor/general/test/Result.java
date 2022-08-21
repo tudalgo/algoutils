e9c8caa0e78441436cfbb5c6abf71572fa736a5e
Dustin Glaser <dustin.glaser@posteo.de>
2022-08-21 19:32:15 +0200

    add methods actual and expectation

--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/Result.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/Result.java
@@ -5,4 +5,10 @@ public interface Result {
     boolean successful();
 
     Test test();
+
+    Object actual();
+
+    default Object expectation() {
+        return test().expectation();
+    }
 }
