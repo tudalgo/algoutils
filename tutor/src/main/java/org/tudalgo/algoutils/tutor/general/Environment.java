0fe9e1efa56ca6eaa3fd1673f0f35acbad05afe4
Dustin Glaser <dustin.glaser@posteo.de>
2022-08-24 15:57:05 +0200

    add Environment

---
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/Environment.java
@@ -0,0 +1,15 @@
+package org.tudalgo.algoutils.tutor.general;
+
+import org.tudalgo.algoutils.tutor.general.stringify.Stringifier;
+import org.tudalgo.algoutils.tutor.general.test.CommentFactory;
+import org.tudalgo.algoutils.tutor.general.test.Result;
+import org.tudalgo.algoutils.tutor.general.test.Test;
+
+public interface Environment {
+
+    CommentFactory<Result<?>> getCommentFactory();
+
+    void setStringifier(Stringifier stringifier);
+
+    Stringifier getStringifier();
+}
