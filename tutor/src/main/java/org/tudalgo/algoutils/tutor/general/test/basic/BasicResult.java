19399d9394ddf4876f32533e572c11c71f1cad1d
Dustin Glaser <dustin.glaser@posteo.de>
2022-09-28 14:29:17 +0200

    modifier

--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/basic/BasicResult.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/basic/BasicResult.java
@@ -8,9 +8,9 @@ import org.tudalgo.algoutils.tutor.general.test.Test;
 
 public abstract class BasicResult<TT extends Test> implements Result<TT> {
 
-    private final Environment environment;
-    private final boolean successful;
-    private final TT test;
+    protected final Environment environment;
+    protected final boolean successful;
+    protected final TT test;
 
     public BasicResult(Environment environment, TT test, boolean successful) {
         this.environment = environment;
