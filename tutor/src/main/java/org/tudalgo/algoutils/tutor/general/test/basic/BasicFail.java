75fc4e5b3c13ba6a58a92d7bfbc3e1c499a7f26c
Dustin Glaser <dustin.glaser@posteo.de>
2022-10-01 13:57:48 +0200

    add builders and factories

--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/basic/BasicFail.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/basic/BasicFail.java
@@ -10,4 +10,28 @@ public class BasicFail extends BasicTest<BasicFail, Nothing, BasicResultOfFail, 
     public BasicFail(Environment environment) {
         super(environment, null);
     }
+
+    public static class Builder extends BasicTest.Builder<BasicFail, Nothing, BasicResultOfFail, NoActual, Builder> implements Fail.Builder<BasicFail, BasicResultOfFail, Builder> {
+
+        public Builder(Environment environment) {
+            super(environment);
+        }
+
+        @Override
+        public BasicFail build() {
+            return new BasicFail(environment);
+        }
+
+        public static class Factory extends BasicTest.Builder.Factory<BasicFail, Nothing, BasicResultOfFail, NoActual, Builder> implements Fail.Builder.Factory<BasicFail, BasicResultOfFail, Builder> {
+
+            public Factory(Environment environment) {
+                super(environment);
+            }
+
+            @Override
+            public Builder builder() {
+                return new Builder(environment);
+            }
+        }
+    }
 }
--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/basic/BasicResultOfFail.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/test/basic/BasicResultOfFail.java
@@ -1,6 +1,7 @@
 package org.tudalgo.algoutils.tutor.general.test.basic;
 
 import org.tudalgo.algoutils.tutor.general.Environment;
+import org.tudalgo.algoutils.tutor.general.test.Result;
 import org.tudalgo.algoutils.tutor.general.test.ResultOfFail;
 import org.tudalgo.algoutils.tutor.general.test.actual.NoActual;
 import org.tudalgo.algoutils.tutor.general.test.expected.Nothing;
@@ -9,5 +10,29 @@ public class BasicResultOfFail extends BasicResult<BasicResultOfFail, NoActual, 
 
     public BasicResultOfFail(Environment environment, BasicFail test, Exception exception) {
         super(environment, test, null, exception);
+    }
+
+    public static class Builder extends BasicResult.Builder<BasicResultOfFail, NoActual, BasicFail, Nothing, Builder> implements ResultOfFail.Builder<BasicResultOfFail, BasicFail, Builder> {
+
+        public Builder(Environment environment) {
+            super(environment);
+        }
+
+        @Override
+        public BasicResultOfFail build() {
+            return new BasicResultOfFail(environment, test, exception);
+        }
+
+        public static class Factory extends BasicResult.Builder.Factory<BasicResultOfFail, NoActual, BasicFail, Nothing, Builder> implements ResultOfFail.Builder.Factory<BasicResultOfFail, BasicFail, Builder> {
+
+            public Factory(Environment environment) {
+                super(environment);
+            }
+
+            @Override
+            public Result.Builder<BasicResultOfFail, NoActual, BasicFail, Nothing, Builder> builder() {
+                return new Builder(environment);
+            }
+        }
     }
 }
