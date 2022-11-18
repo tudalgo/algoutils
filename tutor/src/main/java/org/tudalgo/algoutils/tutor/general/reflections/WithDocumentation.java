2740feb842148b49152d0a7c9d0733eb9b14dbc7
Dustin Glaser <dustin.glaser@posteo.de>
2022-11-18 13:22:21 +0100

    add interface WithDocumentation

---
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/WithDocumentation.java
@@ -0,0 +1,11 @@
+package org.tudalgo.algoutils.tutor.general.reflections;
+
+public interface WithDocumentation {
+
+    /**
+     * <p>Returns the documentation of this object.</p>
+     *
+     * @return the documentation
+     */
+    String documentation();
+}
