7ee415c2e366de6aa95a50330a7e8c18ae0d752a
Dustin Glaser <dustin.glaser@posteo.de>
2022-11-26 21:18:41 +0100

    do not use * imports

--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/BasicTypeLink.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/BasicTypeLink.java
@@ -1,6 +1,12 @@
 package org.tudalgo.algoutils.tutor.general.reflections;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import static java.util.Arrays.stream;
 import static java.util.Collections.unmodifiableList;
