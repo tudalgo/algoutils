d324e9417e0880f45f6ba1e53a0c1d69e46b4e5b
Dustin Glaser <dustin.glaser@posteo.de>
2022-11-28 00:21:09 +0100

    add Assertions4

---
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/assertions/Assertions4.java
@@ -0,0 +1,123 @@
+package org.tudalgo.algoutils.tutor.general.assertions;
+
+import org.eclipse.jdt.core.dom.WhileStatement;
+import org.tudalgo.algoutils.tutor.general.assertions.expected.ExpectedObjects;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtFor;
+import spoon.reflect.code.CtForEach;
+import spoon.reflect.code.CtIf;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.declaration.CtExecutable;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.visitor.Filter;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.tudalgo.algoutils.tutor.general.assertions.expected.Nothing.text;
+
+@SuppressWarnings("unused")
+public class Assertions4 {
+
+    private Assertions4() {
+    }
+
+    public static <T extends CtElement> T assertIsCtElementOfType(
+        Class<T> expected,
+        CtElement actual,
+        Context context,
+        PreCommentSupplier<? super ResultOfObject<Class<? extends CtElement>>> comment
+    ) {
+        Assertions2.<Class<? extends CtElement>>testOfObjectBuilder()
+            .expected(ExpectedObjects.instanceOf(expected, true)).build()
+            .run(actual.getClass())
+            .check(context, comment);
+        //noinspection unchecked
+        return (T) actual;
+    }
+
+    public static CtStatement assertIsOneStatement(
+        CtStatement statement,
+        Context context,
+        PreCommentSupplier<? super ResultOfObject<CtStatement>> comment
+    ) {
+        if (!(statement instanceof CtBlock<?> block)) {
+            return statement;
+        }
+        Assertions2.assertEquals(
+            1,
+            block.getStatements().size(),
+            context,
+            r -> "block contains more than one statement"
+        );
+        return block.getLastStatement();
+    }
+
+    public static CtStatement assertHasElseStatement(
+        CtIf ifStatement,
+        Context context,
+        PreCommentSupplier<? super Result<?, ?, ?, ?>> comment
+    ) {
+        if (ifStatement.getElseStatement() == null) {
+            return Assertions2.fail(
+                text("else statement"),
+                text("no else statement"),
+                context,
+                comment
+            );
+        }
+        return ifStatement.getElseStatement();
+    }
+
+    private static final Filter<CtElement> ITERATIVE_ELEMENTS_FILTER = e -> e instanceof CtFor || e instanceof CtForEach || e instanceof WhileStatement;
+
+    public static void assertIsNotIteratively(
+        CtMethod<?> method,
+        Context context,
+        PreCommentSupplier<? super ResultOfFail> comment
+    ) {
+        if (method.getElements(ITERATIVE_ELEMENTS_FILTER).isEmpty()) {
+            return;
+        }
+        Assertions2.fail(
+            context,
+            comment
+        );
+    }
+
+    public static void assertIsNotRecursively(
+        CtMethod<?> method,
+        Context context,
+        PreCommentSupplier<? super ResultOfFail> comment
+    ) {
+        assertDoesNotCall(method, method, new HashSet<>(), context, comment);
+    }
+
+    private static void assertDoesNotCall(
+        CtExecutable<?> executable,
+        CtMethod<?> calledMethod,
+        Set<CtExecutable<?>> visited,
+        Context context,
+        PreCommentSupplier<? super ResultOfFail> comment
+    ) {
+        executable.getElements(new TypeFilter<>(CtInvocation.class)).forEach(i -> {
+            var e = i.getExecutable().getDeclaration();
+            if (visited.contains(e)) {
+                return;
+            }
+            if (e instanceof CtMethod<?> m && m.equals(calledMethod)) {
+                Assertions2.fail(
+                    context,
+                    comment
+                );
+            }
+            visited.add(e);
+            assertDoesNotCall(e, calledMethod, visited, context, comment);
+        });
+        visited.add(executable);
+    }
+}
+
