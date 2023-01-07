12de73c2ee169546e1bae364a55708dec729a085
nyanyan <46193012+zentox@users.noreply.github.com>
2023-01-07 19:51:25 +0100

    Interface should allow to retrieve imports recursively by checking the method invocations, constructor calls, fields, etc.

--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/BasicConstructorLink.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/BasicConstructorLink.java
@@ -1,14 +1,25 @@
 package org.tudalgo.algoutils.tutor.general.reflections;
 
+import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtElement;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.Arrays.stream;
+import static org.tudalgo.algoutils.tutor.general.ResourceUtils.toShortSignature;
+import static org.tudalgo.algoutils.tutor.general.SpoonUtils.collectConstructorCallImports;
+import static org.tudalgo.algoutils.tutor.general.SpoonUtils.collectFieldImports;
+import static org.tudalgo.algoutils.tutor.general.SpoonUtils.collectImports;
+import static org.tudalgo.algoutils.tutor.general.SpoonUtils.collectMethodInvocationImports;
 
-public class BasicConstructorLink extends BasicLink implements ConstructorLink {
+public class BasicConstructorLink extends BasicLink implements ConstructorLink, WithCtElement {
 
     private static final Map<Constructor<?>, BasicConstructorLink> INSTANCES = new HashMap<>();
 
@@ -16,16 +27,37 @@ public class BasicConstructorLink extends BasicLink implements ConstructorLink {
     private final TypeLink typeLink;
     private final List<BasicTypeLink> parameterTypeLinks;
 
+    /**
+     * The class type link of this constructor link.
+     */
+    private final BasicTypeLink parent;
+
+    /**
+     * The CtElement of this link in the Spoon world.
+     */
+    private CtConstructor<?> element;
+
+    /**
+     * The imports used in this link.
+     */
+    private Set<TypeLink> imports;
+
     private BasicConstructorLink(Constructor<?> constructor) {
         this.constructor = constructor;
         this.typeLink = BasicTypeLink.of(constructor.getDeclaringClass());
         this.parameterTypeLinks = stream(constructor.getParameterTypes()).map(BasicTypeLink::of).toList();
+        this.parent = BasicTypeLink.of(constructor.getDeclaringClass());
+    }
+
+    public BasicConstructorLink(Constructor<?> constructor, BasicMethodLink element) {
+        this(constructor);
     }
 
     /**
      * <p>Returns the constructor link for the given constructor.</p>
      *
      * @param constructor the constructor
+     *
      * @return the constructor link
      */
     public static BasicConstructorLink of(Constructor<?> constructor) {
@@ -64,4 +96,48 @@ public class BasicConstructorLink extends BasicLink implements ConstructorLink {
     public TypeLink type() {
         return typeLink;
     }
+
+    @Override
+    public CtElement getCtElement() {
+        if (element != null) {
+            return element;
+        }
+        return element = (CtConstructor<?>) parent.getCtElement()
+            .getElements(e -> e instanceof CtConstructor<?> c && c.getSignature().equals(toShortSignature(reflection())))
+            .get(0);
+    }
+
+    /**
+     * Collects all the imports used in this link.
+     *
+     * @param element the element to collect the imports from
+     *
+     * @return the collected imports
+     */
+    private Set<TypeLink> collectImportsRecursively(CtElement element) {
+        // Current imports
+        Set<TypeLink> imports = collectImports(element);
+        // Do not visit the same element twice.
+        Set<Link> visited = new HashSet<>(imports);
+        // Imports of the constructor call
+        imports.addAll(collectConstructorCallImports(element, visited));
+        // Imports of the method invocation
+        imports.addAll(collectMethodInvocationImports(element, visited));
+        // Imports of fields
+        imports.addAll(collectFieldImports(element, visited));
+        return imports;
+    }
+
+    @Override
+    public Set<TypeLink> imports(boolean recursive) {
+        if (imports != null) {
+            return imports;
+        }
+        if (!parent.isResourceAvailable()) {
+            return imports = Set.of();
+        }
+        imports = recursive ? collectImportsRecursively(getCtElement()) : collectImports(getCtElement());
+        return imports = Collections.unmodifiableSet(imports);
+    }
+
 }
--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/WithImports.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/reflections/WithImports.java
@@ -5,15 +5,26 @@ import java.util.Set;
 /**
  * An interface for links to types with imports.
  *
- * @author Dustin Glaser
+ * @author Nhan Huynh
  */
 public interface WithImports extends Link {
+
+    /**
+     * Returns the set of imports of this link.
+     *
+     * @param recursive whether to expand the retrieval of imports recursively
+     *
+     * @return the set of imports of this link
+     */
+    Set<TypeLink> imports(boolean recursive);
 
     /**
      * Return all imports used in this link.
      *
      * @return the imports
      */
-    Set<TypeLink> imports();
+    default Set<TypeLink> imports() {
+        return imports(true);
+    }
 
 }
