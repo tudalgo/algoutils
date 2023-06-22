9c48c04eb71ecc8231f6a12b0343089952561ce1
nyanyan <nhanhuynh_online@protonmail.com>
2023-06-22 15:18:15 +0200

    Relocate resources

---
+++ tutor/src/test/resources/org/tudalgo/algoutils/tutor/general/Student.java
@@ -0,0 +1,27 @@
+package org.tudalgo.algoutils.tutor.general;
+
+public class Student {
+    private String name;
+    private int age;
+    private String studentId;
+
+    public Student(String name, int age, String studentId) {
+        this.name = name;
+        this.age = age;
+        this.studentId = studentId;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Student ID: " + studentId);
+    }
+
+    public void study() {
+        System.out.println(name + " is studying.");
+    }
+
+    public void takeExam() {
+        System.out.println(name + " is taking an exam.");
+    }
+}
