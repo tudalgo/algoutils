04aeb5996b95b9c912178e3f8e02bc9002ec8476
nyanyan <nhanhuynh_online@protonmail.com>
2023-06-22 15:18:28 +0200

    Use submission class instead of stdlib

--- tutor/src/test/java/org/tudalgo/algoutils/tutor/general/reflections/BasicMethodLinkTest.java
+++ tutor/src/test/java/org/tudalgo/algoutils/tutor/general/reflections/BasicMethodLinkTest.java
@@ -1,30 +1,65 @@
 package org.tudalgo.algoutils.tutor.general.reflections;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.io.TempDir;
 import spoon.reflect.declaration.CtMethod;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Stream;
 
-@DisplayName("BasicTypeLink")
+import static org.tudalgo.algoutils.tutor.general.ResourcesUtils.FILE_NAMES;
+import static org.tudalgo.algoutils.tutor.general.ResourcesUtils.ROOT_PATH;
+import static org.tudalgo.algoutils.tutor.general.ResourcesUtils.loadClasses;
+
+/**
+ * Defines unit tests for {@link BasicMethodLink}.
+ *
+ * @author Nhan Huynh
+ */
+@DisplayName("BasicMethodLink")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BasicMethodLinkTest {
-    private BasicTypeLink type;
+
+    /**
+     * The shared temporary directory to store the compiled classes.
+     */
+    @TempDir
+    private static Path sharedTempDir;
+
+    /**
+     * The class to test.
+     */
+    Map.Entry<Class<?>, String> clazz;
+
+    /**
+     * The method to test.
+     */
     private BasicMethodLink method;
 
-
     @BeforeAll
-    private void setUp() {
-        type = BasicTypeLink.of(Object.class);
-        method = BasicMethodLink.of(
-            Arrays.stream(Object.class.getMethods())
-                .filter(it -> it.getName().equals("toString"))
-                .findFirst()
-                .orElseThrow()
-        );
+    private void setUp() throws IOException, ClassNotFoundException {
+        Path path = ROOT_PATH.resolve(FILE_NAMES.get(0));
+        clazz = loadClasses(sharedTempDir, path).get(path);
+        method = BasicMethodLink.of(clazz.getKey().getMethods()[0]);
+    }
+
+    @AfterAll
+    public void tearDown() throws IOException {
+        try (Stream<Path> paths = Files.walk(sharedTempDir)) {
+            for (Path path : paths.sorted(Comparator.reverseOrder()).toList()) {
+                Files.delete(path);
+            }
+        }
     }
 
     @DisplayName("getCtElement")
@@ -32,6 +67,6 @@ public class BasicMethodLinkTest {
     public void testGetCtElement() {
         CtMethod<?> ctElement = method.getCtElement();
         Assertions.assertNotNull(ctElement);
-        Assertions.assertEquals("toString", ctElement.getSimpleName());
+        Assertions.assertEquals(clazz.getKey().getMethods()[0].getName(), ctElement.getSimpleName());
     }
 }
--- tutor/src/test/java/org/tudalgo/algoutils/tutor/general/reflections/BasicTypeLinkTest.java
+++ tutor/src/test/java/org/tudalgo/algoutils/tutor/general/reflections/BasicTypeLinkTest.java
@@ -1,20 +1,72 @@
 package org.tudalgo.algoutils.tutor.general.reflections;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.io.TempDir;
+import org.tudalgo.algoutils.tutor.general.io.JavaSubmissionResource;
 import spoon.reflect.declaration.CtType;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.tudalgo.algoutils.tutor.general.ResourcesUtils.FILE_NAMES;
+import static org.tudalgo.algoutils.tutor.general.ResourcesUtils.ROOT_PATH;
+import static org.tudalgo.algoutils.tutor.general.ResourcesUtils.loadClasses;
+
+/**
+ * Defines unit tests for {@link BasicTypeLink}.
+ *
+ * @author Nhan Huynh
+ */
 @DisplayName("BasicTypeLink")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BasicTypeLinkTest {
 
-    private final BasicTypeLink type = BasicTypeLink.of(Object.class);
+    /**
+     * The shared temporary directory to store the compiled classes.
+     */
+    @TempDir
+    private static Path sharedTempDir;
+
+    /**
+     * The class to test.
+     */
+    Map.Entry<Class<?>, String> clazz;
+
+    /**
+     * The type link to test.
+     */
+    private BasicTypeLink type;
+
+    @BeforeAll
+    private void setUp() throws IOException, ClassNotFoundException {
+        Path path = ROOT_PATH.resolve(FILE_NAMES.get(0));
+        clazz = loadClasses(sharedTempDir, path).get(path);
+        type = BasicTypeLink.of(clazz.getKey());
+    }
+
+    @AfterAll
+    public void tearDown() throws IOException {
+        try (Stream<Path> paths = Files.walk(sharedTempDir)) {
+            for (Path path : paths.sorted(Comparator.reverseOrder()).toList()) {
+                Files.delete(path);
+            }
+        }
+    }
 
     @DisplayName("getCtElement")
     @Test
     public void testGetCtElement() {
         CtType<?> ctElement = type.getCtElement();
         Assertions.assertNotNull(ctElement);
-        Assertions.assertEquals("java.lang.Object", ctElement.getQualifiedName());
+        Assertions.assertEquals(clazz.getKey().getName(), ctElement.getQualifiedName());
     }
 }
