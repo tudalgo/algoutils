87efa868f5bc3c52e2b0c726bca6cfe9383542af
Dustin Glaser <dustin.glaser@posteo.de>
2023-11-29 14:05:55 +0100

    add method listOfCtParametersToTypeLinks

--- tutor/src/main/java/org/tudalgo/algoutils/tutor/general/Utils.java
+++ tutor/src/main/java/org/tudalgo/algoutils/tutor/general/Utils.java
@@ -1,6 +1,13 @@
 package org.tudalgo.algoutils.tutor.general;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
+
 import org.sourcegrade.jagr.api.testing.extension.TestCycleResolver;
+import org.tudalgo.algoutils.tutor.general.reflections.BasicTypeLink;
+import org.tudalgo.algoutils.tutor.general.reflections.TypeLink;
+import spoon.reflect.declaration.CtParameter;
 
 public class Utils {
 
@@ -18,5 +25,15 @@ public class Utils {
      */
     public static boolean isJagrRun() {
         return TestCycleResolver.getTestCycle() != null;
+    }
+
+    /**
+     * Transforms the list of {@link CtParameter}s into a list of corresponding {@link TypeLink}s.
+     *
+     * @param list the list of {@link CtParameter}s
+     * @return the list of {@link TypeLink}s
+     */
+    public static List<TypeLink> listOfCtParametersToTypeLinks(List<CtParameter<?>> list) {
+        return list.stream().map(e -> BasicTypeLink.of(e.getType().getActualClass())).collect(toUnmodifiableList());
     }
 }
